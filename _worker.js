0941a52d5a7f2bf71c0db5826ce02d838e402996
liangxiaoshi666 <129644992+liangxiaoshi666@users.noreply.github.com>
2024-07-21 21:38:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'da5c8927-8793-4bef-bfb7-f0f0fb323fc7';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
